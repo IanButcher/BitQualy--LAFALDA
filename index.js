315963daab4456a6ed92a6aa145d4a66cfb58465
cele romero <115805225+Celeromero0808@users.noreply.github.com>
2024-09-13 11:55:00 -0300

    login

---
+++ index.js
@@ -0,0 +1,28 @@
+// Librerias
+const express = require('express')
+const ejs = require('ejs')
+
+// Server config
+const app = express()
+const puerto = 3000
+app.set('view engine', 'ejs')
+
+// Path configs (configuracion para no poner ./views/archivo.ejs)
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static('public'))
+
+// Rutas
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+app.get('/login', (req,res)=>{
+    res.render('login')
+})
+
+
+app.listen(puerto, ()=>{
+    console.log('Servidor abierto')
+    console.log(puerto)
+})
