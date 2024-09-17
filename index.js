122ee37b73ee5798818f2bae4f8d52888f29939b
IanButcher <115805201+IanButcher@users.noreply.github.com>
2024-09-17 14:31:30 -0300

    Prueba Conexion Mongo

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 const express = require('express')
 const ejs = require('ejs')
 const arrayEmpleados = require('./seedEmpleados')
+const mongoose = require('mongoose')
 
 // Server config
 const app = express()
 const puerto = 3000
 app.set('view engine', 'ejs')
+
+// Mongoose conect
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/bitqualyPrueba')
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+main().catch(err => console.log(err))
 
 // Path configs (configuracion para no poner ./views/archivo.ejs)
 const path = require('path')
