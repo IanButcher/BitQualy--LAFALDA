2078faca77509464fbb7f49c4c07d378e36655d5
IanButcher <115805201+IanButcher@users.noreply.github.com>
2024-09-19 12:30:46 -0300

    Empleado Schema Modificacion de prueba

--- Schemas/empleadoSchema.js
+++ Schemas/empleadoSchema.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose')
-const bcryptjs = require('bcryptjs')
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
 
-
+// Define el esquema para empleado
 const empleadoSchema = new mongoose.Schema({
     nombre: {
         type: String,
@@ -18,16 +18,41 @@ const empleadoSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
-    evaluacionesAsignadas: [],
-    evaluacionesCompletadas: [],
+    evaluacionesAsignadas: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Evaluacion' // Reemplaza con el modelo correcto si tienes uno
+        }
+    ],
+    evaluacionesCompletadas: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Evaluacion' // Reemplaza con el modelo correcto si tienes uno
+        }
+    ],
     password: {
         type: String,
         required: true,
-        min: 6,
-        max: 16
+        minlength: 6,
+        maxlength: 16
     }
-})
+});
 
-const Empleado = mongoose.model('Empleado', empleadoSchema)
+// Middleware para encriptar la contraseña antes de guardar
+empleadoSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        try {
+            const salt = await bcryptjs.genSalt(10);
+            this.password = await bcryptjs.hash(this.password, salt);
+            next();
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        next();
+    }
+});
 
-module.exports = Empleado
+const Empleado = mongoose.model('Empleado', empleadoSchema);
+
+module.exports = Empleado;
