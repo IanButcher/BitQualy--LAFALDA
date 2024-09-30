e3d8e7c5c67bf66e417c0228b58ca98b534d16ac
IanButcher <115805201+IanButcher@users.noreply.github.com>
2024-09-30 12:13:05 -0300

    Display preguntas del formulario

--- routes/evaluacionRoutes.js
+++ routes/evaluacionRoutes.js
@@ -22,11 +22,41 @@ router.get('/evaluaciones', async (req, res) => {
 // GET route --> Mostrar evaluacion especifica
 router.get('/evaluaciones/new', async (req, res) => {
     try {
-        const formularios = await Formulario.find()
+        const formularios = await Formulario.find({ isActive: true }).populate('questions')
         res.render('evals/new', { formularios })
     } catch (error) {
         console.error('Error fetching forms:', error)
         res.status(500).send('Error interno del servidor')
+    }
+})
+
+//app.get('/formularios/:id/preguntas', async (req, res) => {
+    //try {
+      //  const formulario = await Formulario.findById(req.params.id).populate('preguntas');
+        //res.json({ preguntas: formulario.preguntas });
+    //} catch (error) {
+      //  console.error('Error fetching preguntas:', error);
+        //res.status(500).json({ error: 'Server Error' });
+    //}
+//})
+
+// GET route --> Mostrar las preguntas para la evaluación
+router.get('/evaluaciones/answer/:id', async (req, res) => {
+    console.log('GET /evaluaciones/answer/:id reached')
+    try {
+        const { id } = req.params;  // formulario ID
+        const { empleado } = req.query;  // empleado from the query params
+        console.log(`Formulario ID: ${id}, Empleado: ${empleado}`)
+        const formulario = await Formulario.findById(id).populate('questions')
+        if (!formulario || formulario.isActive != true) {
+            return res.status(404).send('Formulario no encontrado');
+        }
+
+        // Render a new page with the preguntas and the selected empleado
+        res.render('evals/awnser', { formulario, empleado });
+    } catch (error) {
+        console.error('Error fetching formulario:', error);
+        res.status(500).send('Error interno del servidor');
     }
 });
 
@@ -42,7 +72,7 @@ router.post('/evaluacion/save-evaluacion', async (req, res) => {
             return res.status(404).send('Formulario no encontrado')
         }
 
-        // 
+        // Crear instancia evaluacion
         const nuevaEvaluacion = new Evaluacion({
             formulario: formulario._id,
             empleado: empleado,  
